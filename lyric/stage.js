349644b053f0ca2cde21e5f8a3f655170305bab8
Tim S <tims@corshambaptists.org>
2024-10-13 09:14:41 +0100

    wip: full scale images

--- lyric/stage.js
+++ lyric/stage.js
@@ -1,3 +1,20 @@
+function renderImage(host) {
+  fetch(`http://${host}:4316/api/v2/core/live-image`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+	    console.error(data);
+      document.getElementById('template-container').innerHTML = data.binary_image;
+    })
+    .catch(error => {
+      console.error('Fetch error:', error);
+    });
+}
+
 function renderSlide(host) {
   fetch(`http://${host}:4316/api/v2/controller/live-item`)
     .then(response => {
@@ -20,6 +37,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const host = window.location.hostname;
   const websocket_port = 4317;
   renderSlide(host);
+  //	renderImage(host);
   
   ws = new WebSocket(`ws://${host}:${websocket_port}`);
   ws.onmessage = (event) => {
@@ -29,6 +47,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
       console.log(state);
       renderSlide(host);
+//	renderImage(host);
     };
     reader.readAsText(event.data);
   };
